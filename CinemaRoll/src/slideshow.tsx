02fa7f2b48cd9632ac4c0c220b721debaf3e7c06
Lisec, George <glisec@iu.edu>
2024-03-06 12:47:20 -0500

    slideshow buttons work

--- CinemaRoll/src/slideshow.tsx
+++ CinemaRoll/src/slideshow.tsx
@@ -25,13 +25,63 @@ export default function BoxOffice({ images }: BoxOfficeProps) {
     });
   }
 
+  //March 6 - buttons work, need to make it look sexy and get the images to stack
   return (
     <div className="">
-      <div className="flex justify-start bg-gray-500 rounded-3xl m-10">
-        <img className="object-fit:scale-down rounded-3xl" src={images[index]}></img>
+      <div className="flex items-center justify-start bg-gray-500 rounded-3xl m-10">
+        <button
+          className="flex w-10 h-10 bg-gray-400 rounded-sm font-semibold hover:bg-orange-500 items-center justify-center m-10"
+          onClick={previousImage}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
+            />
+          </svg>
+        </button>
+        <img
+          className="object-contain h-80 w-80 rounded-3xl m-4"
+          src={images[index]}
+        ></img>
+        <button
+          className="flex w-10 h-10 bg-gray-400 rounded-sm font-semibold hover:bg-orange-500 items-center justify-center"
+          onClick={nextImage}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+            />
+          </svg>
+        </button>
+        <div className="flex justify-end column flex-wrap gap-x-4">
+          <img
+            className="flex justify-end object-contain h-20 w-20 rounded-3xl m-4"
+            src={images[index + 1]}
+          ></img>
+          <img
+            className="flex justify-end object-contain h-20 w-20 rounded-3xl m-4"
+            src={images[index + 2]}
+          ></img>
+        </div>
       </div>
-      <button className="h-5 bg-white" onClick={previousImage}></button>
-      <button className="h-5" onClick={nextImage}></button>
     </div>
   );
 }
